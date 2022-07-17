faa86cfc204b700151221497edebf4275afd871b
Luís Simas <luissimas@protonmail.com.br>
2022-07-17 18:31:17 -0300

    feat: linters

---
+++ jest.config.ts
@@ -0,0 +1,26 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  // Automatically clear mock calls, instances and results before every test
+  clearMocks: true,
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'v8',
+
+  // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
+  // Enables tsconfig-paths in jest
+  moduleNameMapper: {
+    '@errors/(.*)': '<rootDir>/src/application/domain/errors/$1',
+    '@errors': '<rootDir>/src/application/domain/errors',
+    '@entities/(.*)': '<rootDir>/src/application/domain/entities/$1',
+    '@infrastructure/(.*)': '<rootDir>/src/infrastructure/$1',
+  },
+
+  // A preset that is used as a base for Jest's configuration
+  preset: 'ts-jest',
+  // The glob patterns Jest uses to detect test files
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+}
