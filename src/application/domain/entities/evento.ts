a534d2f4b0db1036f02154ba6109a261f0d7ca1b
Luís Simas <luissimas@protonmail.com.br>
2022-07-18 10:14:16 -0300

    fix: linting

--- src/application/domain/entities/evento.ts
+++ src/application/domain/entities/evento.ts
@@ -3,28 +3,27 @@ import { Aluno } from '@entities/aluno'
 import { Local } from '@entities/local'
 
 export enum EventoStatusEnum {
-	A_ACONTECER,
-	ACONTECENDO,
-	CANCELADO,
+  A_ACONTECER,
+  ACONTECENDO,
+  CANCELADO,
 }
 
 export class Evento {
-	public readonly id: string
-	public readonly nome: string
-	public readonly data: Date
-	public readonly organizadores: Aluno[]
-	public readonly status: EventoStatusEnum
-	public readonly local: Local
-	public readonly palestrantes: string[]
+  public readonly id: string
+  public readonly nome: string
+  public readonly data: Date
+  public readonly organizadores: Aluno[]
+  public readonly status: EventoStatusEnum
+  public readonly local: Local
+  public readonly palestrantes: string[]
 
-	constructor(props: Omit<Evento, 'id'>, id?: string) {
-		this.id = id || uuid()
-		this.nome = props.nome
-		this.data = props.data
-		this.organizadores = props.organizadores
-		this.status = props.status
-		this.local = props.local
-		this.palestrantes = props.palestrantes
-	}
+  constructor(props: Omit<Evento, 'id'>, id?: string) {
+    this.id = id || uuid()
+    this.nome = props.nome
+    this.data = props.data
+    this.organizadores = props.organizadores
+    this.status = props.status
+    this.local = props.local
+    this.palestrantes = props.palestrantes
+  }
 }
-
