0ead8b6cce6c0377431302f98ca6424fb84c7434
Luís Simas <luissimas@protonmail.com>
2022-08-25 16:07:49 -0300

    feat: using RS256 to sign jwt tokens

--- src/infra/auth/jwtAuthService.ts
+++ src/infra/auth/jwtAuthService.ts
@@ -4,12 +4,20 @@ import { sign, verify, decode } from 'jsonwebtoken'
 
 export class JwtAuthService implements IAuthService {
   async generateToken(payload: TokenPayload): Promise<string> {
-    return sign(payload, 'SECRET', { expiresIn: '1h' })
+    if (!process.env.PRIVATE_AUTH_KEY) throw new Error('Private auth key not found')
+
+    const privateKey = process.env.PRIVATE_AUTH_KEY.replace(/\\n/g, '\n')
+
+    return sign(payload, privateKey, { expiresIn: '1h', algorithm: 'RS256' })
   }
 
   async validateToken(token: string): Promise<boolean> {
     try {
-      verify(token, 'SECRET')
+      if (!process.env.PUBLIC_AUTH_KEY) throw new Error('Public auth key not found')
+
+      const publicKey = process.env.PUBLIC_AUTH_KEY.replace(/\\n/g, '\n')
+
+      verify(token, publicKey)
       return true
     } catch (error) {
       throw new UnauthorizedError('Token inválido')
