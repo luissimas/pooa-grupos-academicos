d661aabf935c5140dd21a7ab10b5971d9c40374b
Luís Simas <luissimas@protonmail.com>
2022-08-28 15:38:41 -0300

    feat: ListAcademicGroupsByUserUsecase

---
+++ src/application/usecases/academicGroup/listAcademicGroupsByUser.ts
@@ -0,0 +1,30 @@
+import { AcademicGroupDTO } from '@application/dtos/academicGroup'
+import { IAcademicGroupRepository } from '@application/repositories/academicGroupRepository'
+import { IUserRepository } from '@application/repositories/userRepository'
+import { EntityNotFound } from '@domain/errors'
+import { IUsecase } from '..'
+
+export type ListAcademicGroupsByUserParams = {
+  userId: string
+}
+
+export type ListAcademicGroupsByUserResult = AcademicGroupDTO[]
+
+export interface IListAcademicGroupsByUserUsecase
+  extends IUsecase<ListAcademicGroupsByUserParams, ListAcademicGroupsByUserResult> {}
+
+export class ListAcademicGroupsByUserUsecase implements IListAcademicGroupsByUserUsecase {
+  constructor(
+    private readonly academicGroupRepository: IAcademicGroupRepository,
+    private readonly userRepository: IUserRepository
+  ) {}
+
+  async execute({ userId }: ListAcademicGroupsByUserParams): Promise<ListAcademicGroupsByUserResult> {
+    const existingUser = await this.userRepository.getById(userId)
+
+    if (!existingUser) throw new EntityNotFound('User')
+
+    const academicGroups = await this.academicGroupRepository.getByUser(userId)
+    return academicGroups
+  }
+}
