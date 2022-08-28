e1c40f20dbbedb5a6fb73a168828d6309476cab4
Luís Simas <luissimas@protonmail.com>
2022-08-28 15:56:27 -0300

    feat: academic group repository getByUser

--- src/infra/repositories/academicGroup/memoryAcademicGroupRepository.ts
+++ src/infra/repositories/academicGroup/memoryAcademicGroupRepository.ts
@@ -1,18 +1,21 @@
-import { AcademicGroup } from "@entities/academicGroup"
-import { IAcademicGroupRepository } from "@repositories/academicGroupRepository"
+import { AcademicGroup } from '@entities/academicGroup'
+import { IAcademicGroupRepository } from '@repositories/academicGroupRepository'
 
 export class MemoryAcademicGroupRepository implements IAcademicGroupRepository {
   private static academicGroups: AcademicGroup[] = []
 
   async create(academicGroup: AcademicGroup): Promise<void> {
-    MemoryAcademicGroupRepository.academicGroups = [
-      ...MemoryAcademicGroupRepository.academicGroups, academicGroup
-    ]
+    MemoryAcademicGroupRepository.academicGroups = [...MemoryAcademicGroupRepository.academicGroups, academicGroup]
   }
   async list(): Promise<AcademicGroup[]> {
     return MemoryAcademicGroupRepository.academicGroups
   }
   async getById(id: string): Promise<AcademicGroup | undefined> {
     return MemoryAcademicGroupRepository.academicGroups.find(group => group.id === id)
+  }
+  async getByUser(idUser: string): Promise<AcademicGroup[]> {
+    return MemoryAcademicGroupRepository.academicGroups.filter(
+      group => group.sponsor.id === idUser || group.members.filter(member => member.id === idUser).length > 0
+    )
   }
 }
