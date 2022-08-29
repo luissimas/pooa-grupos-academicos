b8136c82d00bb2e8af147114d3acef6fbcd86316
Luís Simas <luissimas@protonmail.com.br>
2022-08-29 09:24:22 -0300

    fix: moving docs to correct route

--- src/http/routes/academicGroup.ts
+++ src/http/routes/academicGroup.ts
@@ -1,11 +1,78 @@
-import { CreateAcademicGroupControllerFactory } from "@factories/controller/academicGroup/createAcademicGroupControllerFactory"
-import { adaptController } from "@adapters/expressControllerAdapter"
-import { Router } from "express"
+import { adaptController } from '@adapters/expressControllerAdapter'
+import { CreateAcademicGroupControllerFactory } from '@factories/controller/academicGroup/createAcademicGroupControllerFactory'
+import { AuthMiddlewareFactory } from '@factories/middlewares/authMiddlewareFactory'
+import { adaptMiddleware } from '@http/adapters/expressMiddlewareAdapter'
+import { Router } from 'express'
 
+const authMiddleware = new AuthMiddlewareFactory().createMiddleware()
 const createAcademicGroupController = new CreateAcademicGroupControllerFactory().createController()
 
 const router = Router()
 
+router.use('/', adaptMiddleware(authMiddleware))
+
+/**
+ * @swagger
+ * /academicGroup:
+ *   post:
+ *     summary: Cadastro de grupos acadêmicos.
+ *     description: Cria um novo grupo acadêmico no sistema.
+ *     tags:
+ *       - Grupo acadêmico
+ *     requestBody:
+ *       content:
+ *         'application/json':
+ *           schema:
+ *            properties:
+ *              name:
+ *                type: string
+ *              description:
+ *                type: string
+ *              foundationDate:
+ *                type: string
+ *                format: date-time
+ *              department:
+ *                type: object
+ *                properties:
+ *                  name:
+ *                    type: string
+ *                  abbreviation:
+ *                    type: string
+ *              sponsorId:
+ *                type: string
+ *                format: uuid
+ *              members:
+ *                type: array
+ *                items:
+ *                  type: string
+ *                  format: uuid
+ *              maxMembers:
+ *                type: number
+ *            required:
+ *              - name
+ *              - description
+ *              - foundationDate
+ *              - department
+ *              - sponsorId
+ *              - members
+ *              - maxMembers
+ *     responses:
+ *      '200':
+ *        description: Grupo acadêmico criado com sucesso
+ *        content:
+ *          application/json:
+ *            schema:
+ *              properties:
+ *                id:
+ *                  type: string
+ *                  description: Identificador do grupo acadêmico criado.
+ *      '400':
+ *        description: Campos da requisição inválidos
+ *      '404':
+ *        description: Usuário não encontrado
+ *      '500':
+ *        description: Erro interno no servidor
+ */
 router.post('/', adaptController(createAcademicGroupController))
 
 export { router }
