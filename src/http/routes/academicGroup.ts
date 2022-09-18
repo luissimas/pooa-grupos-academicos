57a028834febfe75390b226672d7b849a02eaf5a
Vitor Fabris <fabrisvitor@estudante.ufscar.br>
2022-09-18 17:18:07 -0300

    feat: list academic group by id route

--- src/http/routes/academicGroup.ts
+++ src/http/routes/academicGroup.ts
@@ -1,6 +1,7 @@
 import { adaptController } from '@adapters/expressControllerAdapter'
 import { AddAcademicGroupMemberControllerFactory } from '@factories/controller/academicGroup/addAcademicGroupMemberControllerFactory'
 import { CreateAcademicGroupControllerFactory } from '@factories/controller/academicGroup/createAcademicGroupControllerFactory'
+import { ListAcademicGroupByIdControllerFactory } from '@factories/controller/academicGroup/listAcademicGroupByIdControllerFactory'
 import { ListAcademicGroupMembersControllerFactory } from '@factories/controller/academicGroup/listAcademicGroupMembersControllerFactory'
 import { AuthMiddlewareFactory } from '@factories/middlewares/authMiddlewareFactory'
 import { adaptMiddleware } from '@http/adapters/expressMiddlewareAdapter'
@@ -10,6 +11,7 @@ const authMiddleware = AuthMiddlewareFactory.createMiddleware()
 const createAcademicGroupController = CreateAcademicGroupControllerFactory.createController()
 const listAcademicGroupMembersController = ListAcademicGroupMembersControllerFactory.createController()
 const addAcademicGroupMemberController = AddAcademicGroupMemberControllerFactory.createController()
+const listAcademicGroupByIdController = ListAcademicGroupByIdControllerFactory.createController()
 
 const router = Router()
 
@@ -149,5 +151,38 @@ router.get('/:academicGroupId/member', adaptController(listAcademicGroupMembersC
  *        description: Erro interno no servidor
  */
 router.put('/:academicGroupId/member/new', adaptController(addAcademicGroupMemberController))
+
+/**
+ * @swagger
+ * /academicGroup/:academicGroupId/member:
+ *   get:
+ *     summary: Listagem de membros de um grupo acadêmico.
+ *     description: Lista todos os membros de um grupo acadêmico dado seu `groupId`.
+ *     tags:
+ *       - Grupo acadêmico
+ *     parameters:
+ *       - name: academicGroupId
+ *         in: path
+ *         schema:
+ *           type: string
+ *     responses:
+ *      '200':
+ *        description: Membros listados com sucesso
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                oneOf:
+ *                  - $ref: '#/components/schemas/Student'
+ *                  - $ref: '#/components/schemas/Professor'
+ *      '400':
+ *        description: Campos inválidos
+ *      '404':
+ *        description: Grupo acadêmico não encontrado
+ *      '500':
+ *        description: Erro interno no servidor
+ */
+ router.get('/:academicGroupId', adaptController(listAcademicGroupByIdController))
 
 export { router }
