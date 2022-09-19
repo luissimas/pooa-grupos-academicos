8fbfc67ce5f84d49bef0cef83b928ba7b5113dd6
Leticia Marchezi <39420862+letMarchezi@users.noreply.github.com>
2022-09-19 12:56:13 -0300

    feat: adding listEventByAcademicGroup Controller

---
+++ src/http/controllers/event/listEventByAcademicGroupController.ts
@@ -0,0 +1,31 @@
+import {
+  IListEventByAcademicGroupUsecase,
+  ListEventByAcademicGroupResult,
+} from '@application/usecases/event/ListEventByAcademicGroupUsecase'
+import { InvalidFieldError } from '@domain/errors'
+import { HttpRequest, HttpResponse, IHttpController } from '@http'
+import Joi from 'joi'
+
+export class ListEventByAcademicGroupController implements IHttpController {
+  constructor(private readonly listEventByAcademicGroupUsecase: IListEventByAcademicGroupUsecase) {}
+
+  async handle(request: HttpRequest): Promise<HttpResponse<ListEventByAcademicGroupResult>> {
+    const { error, value } = this.validateParams(request.params)
+
+    if (error) throw new InvalidFieldError(error.details[0].path[0] as string, error.details[0].message)
+
+    const result = await this.listEventByAcademicGroupUsecase.execute(value)
+    return {
+      status: 200,
+      data: result,
+    }
+  }
+
+  private validateParams(params: any) {
+    return Joi.object()
+      .keys({
+        academicGroupId: Joi.string().uuid().required(),
+      })
+      .validate(params)
+  }
+}
