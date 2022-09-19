20fd32730e0f6094bc14f2b9bccf021242fcf1c0
Luís Simas <luissimas@protonmail.com.br>
2022-09-19 13:56:29 -0300

    fix: missing dependency on factory

--- src/factories/usecase/academicGroup/updateAcademicGroupSponsorUsecaseFactory.ts
+++ src/factories/usecase/academicGroup/updateAcademicGroupSponsorUsecaseFactory.ts
@@ -4,6 +4,7 @@ import {
 } from '@application/usecases/academicGroup/updateAcademicGroupSponsorUsecase'
 import { MemoryAcademicGroupRepository } from '@infra/repositories/academicGroup/memoryAcademicGroupRepository'
 import { ApiClassEnrollmentRepository } from '@infra/repositories/classEnrollment/apiClassEnrollmentRepository'
+import { ApiLibraryReservationRepository } from '@infra/repositories/libraryReservation/apiLibraryReservationRepository'
 import { MemoryUserRepository } from '@infra/repositories/user/memoryUserRepository'
 
 export abstract class UpdateAcademicGroupSponsorUsecaseFactory {
@@ -11,11 +12,13 @@ export abstract class UpdateAcademicGroupSponsorUsecaseFactory {
     const academicGroupRepository = MemoryAcademicGroupRepository.getInstance()
     const userRepository = MemoryUserRepository.getInstance()
     const classEnrollmentRepository = ApiClassEnrollmentRepository.getInstance()
+    const libraryReservationRepository = ApiLibraryReservationRepository.getInstance()
 
     const updateAcademicGroupSponsorUsecase = new UpdateAcademicGroupSponsorUsecase(
       academicGroupRepository,
       userRepository,
-      classEnrollmentRepository
+      classEnrollmentRepository,
+      libraryReservationRepository
     )
 
     return updateAcademicGroupSponsorUsecase
