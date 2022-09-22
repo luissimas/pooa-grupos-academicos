60f6257fa55d6d506172ea25ab801df38f725ca5
Luís Simas <luissimas@protonmail.com.br>
2022-09-22 13:23:31 -0300

    feat: name filter on academic groups listing

--- src/application/repositories/academicGroupRepository.ts
+++ src/application/repositories/academicGroupRepository.ts
@@ -2,7 +2,7 @@ import { AcademicGroup } from '@entities/academicGroup'
 
 export interface IAcademicGroupRepository {
   create: (academicGroup: AcademicGroup) => Promise<void>
-  list: () => Promise<AcademicGroup[]>
+  list: (name?: string) => Promise<AcademicGroup[]>
   getById: (id: string) => Promise<AcademicGroup | undefined>
   getByUser: (idUser: string) => Promise<AcademicGroup[]>
   update: (academicGroup: AcademicGroup) => Promise<void>
--- src/http/controllers/academicGroup/listAcademicGroupsController.ts
+++ src/http/controllers/academicGroup/listAcademicGroupsController.ts
@@ -1,15 +1,28 @@
-import { IListAcademicGroupsUsecase, ListAcademicGroupsResult } from '@usecases/academicGroup/listAcademicGroupsUsecase'
+import { InvalidFieldError } from '@domain/errors'
 import { HttpRequest, HttpResponse, IHttpController } from '@http'
+import { IListAcademicGroupsUsecase, ListAcademicGroupsResult } from '@usecases/academicGroup/listAcademicGroupsUsecase'
+import Joi from 'joi'
 
 export class ListAcademicGroupsController implements IHttpController {
   constructor(private readonly listAcademicGroupsUsecase: IListAcademicGroupsUsecase) {}
 
-  async handle(_request: HttpRequest): Promise<HttpResponse<ListAcademicGroupsResult>> {
-    const result = await this.listAcademicGroupsUsecase.execute()
+  async handle(request: HttpRequest): Promise<HttpResponse<ListAcademicGroupsResult>> {
+    const { error, value } = this.validateQuery(request.query)
 
+    if (error) throw new InvalidFieldError(error.details[0].path[0] as string, error.details[0].message)
+
+    const result = await this.listAcademicGroupsUsecase.execute(value)
     return {
       status: 200,
       data: result,
     }
+  }
+
+  private validateQuery(query: any) {
+    return Joi.object()
+      .keys({
+        name: Joi.string(),
+      })
+      .validate(query)
   }
 }
